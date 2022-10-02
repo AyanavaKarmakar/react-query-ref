b51dfaa713b146bde89e155ad4064a0e8bada79c
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-03 02:05:11 +0530

    feat: setup axios

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
+  </QueryClientProvider>,
 )
 
 reportWebVitals()
