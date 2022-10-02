21175ba355f79b732b3e4a07e01e0e27bfff29c6
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-03 02:10:42 +0530

    feat: remove dummy data

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,17 +1,3 @@
-/**
- * placeholder object
- * @see https://course-api.com/react-store-products
- */
-const product = {
-  id: 'recZkNf2kwmdBcqd0',
-  name: 'accent chair',
-  price: 25999,
-  company: 'marcos',
-  description:
-    'Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge',
-  category: 'office',
-}
-
 interface Product {
   id?: string
   name?: string
